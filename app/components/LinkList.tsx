43f31638cca06cc63a4764986604bcd76045ee9f
Arturas <mixmln14@gmail.com>
2024-06-01 16:06:01 +0300

    more footer

--- app/components/LinkList.tsx
+++ app/components/LinkList.tsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React from 'react';
 
 type LinkListProps = {
-  title: string;
-  list: { label: string; href: string }[];
+	title: string;
+	list: { label: string; href: string }[];
+	style: any;
 };
 
-const LinkList = ({ list, title }: LinkListProps) => {
-  return (
-    <div className='link-list-container'>
-      <h2 className='link-list-title'>{title}</h2>
-      <ul className='link-list-list'>
-        {list
-          ? list.map((item, i) => (
-              <li key={i}>
-                <a href={item.href}>{item.label}</a>
-              </li>
-            ))
-          : null}
-      </ul>
-    </div>
-  );
+const LinkList = ({ list, title, style }: LinkListProps) => {
+	return (
+		<div
+			className='link-list-container'
+			style={style}>
+			<h2 className='link-list-title'>{title}</h2>
+			<ul className='link-list-list'>
+				{list
+					? list.map((item, i) => (
+							<li key={i}>
+								<a href={item.href}>{item.label}</a>
+							</li>
+					  ))
+					: null}
+			</ul>
+		</div>
+	);
 };
 
 export default LinkList;
